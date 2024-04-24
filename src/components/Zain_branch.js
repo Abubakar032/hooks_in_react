88410750eac3b2bd4ae2bd80ecd76b40f9cd1221
zain ijaz <zainijaz255@gmail.com>
2024-04-24 04:19:28 -0700

    add compo

---
+++ src/components/Zain_branch.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const zain_branch = () => {
+  return (
+    <>
+    <div>
+        <h1>My name is zain </h1>
+    </div>
+    </>
+  )
+}
+
+export default zain_branch
